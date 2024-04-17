0298bd64cff1b6a435d880d568e290b67fe8ea20
Zeus21z <leonpereyradenzel@gmail.com>
2024-04-17 11:00:13 -0400

    agregar

--- js/operadores.js
+++ js/operadores.js
@@ -1,4 +1,4 @@
-/*let a = 10;
+let a = 10;
 let b = 5;
 
 console.log(a);
@@ -19,50 +19,6 @@ if ((a%2)==0)
 else 
     console.log("a es IMPAR");
 console.log(a+b+Number("5"));
-*/
+
 
 // Función para generar un número aleatorio entre un rango dado
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  // Array para almacenar los datos
-  let estudiantes = [];
-  
-  // Generar los datos para 10 estudiantes
-  for (let i = 0; i < 10; i++) {
-    let nombre = "Estudiante " + (i + 1);
-    let nota = getRandomInt(20, 100);
-    estudiantes.push({ nombre: nombre, nota: nota });
-  }
-  
-  // Mostrar los datos de los estudiantes
-  console.log("Datos de los estudiantes:");
-  console.log(estudiantes);
-  
-  // Calcular la nota mayor, la menor y el promedio
-  let notaMayor = estudiantes[0].nota;
-  let notaMenor = estudiantes[0].nota;
-  let sumaNotas = estudiantes[0].nota;
-  
-  for (let i = 1; i < estudiantes.length; i++) {
-    let nota = estudiantes[i].nota;
-    sumaNotas += nota;
-  
-    if (nota > notaMayor) {
-      notaMayor = nota;
-    }
-  
-    if (nota < notaMenor) {
-      notaMenor = nota;
-    }
-  }
-  
-  let promedio = sumaNotas / estudiantes.length;
-  
-  // Mostrar los resultados
-  console.log("\nResultados:");
-  console.log("Nota mayor:", notaMayor);
-  console.log("Nota menor:", notaMenor);
-  console.log("Promedio:", promedio.toFixed(2));
-  
